c82a27d3633bf91644ccee97c36b7b95a53916c1
surajsrivastava796 <108386309+surajsrivastava796@users.noreply.github.com>
2026-07-10 15:49:38 +0530

    Update provider.tf

--- Environment/dev/provider.tf
+++ Environment/dev/provider.tf
@@ -5,8 +5,14 @@ terraform {
       version = "4.63.0"
     }
   }
-}
 
+backend "azurerm" {
+  resource_group_name =  "demo-GA-dev"
+  storage_account_name = "demogastgdev"                              # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+  container_name       = "demo-ga-cont"                               # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+  key                  = "dev.terraform.tfstate"                     # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+}
+}
 provider "azurerm" {
   subscription_id = "02846caf-bf53-491f-897c-a322a1632bf8"
   features {
